3103df1e6147b47cfd6c1796896a3a0c0687586d
stanica <robert@stani.ca>
2016-04-11 15:35:31 -0400

    Got laid off starting new project here we go.

---
+++ client/app/admin/admin.router.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('referralApp.admin')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminController',
+        controllerAs: 'admin',
+        authenticate: 'admin'
+      });
+  });
