1e15692661cd2fd08d8c7d942da0c8643a809862
stanica <robert@stani.ca>
2016-04-11 15:35:31 -0400

    Got laid off starting new project here we go.

---
+++ client/app/admin/admin.module.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('referralApp.admin', [
+  'referralApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('referralApp', [
+  'referralApp.auth',
+  'referralApp.admin',
+  'referralApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
